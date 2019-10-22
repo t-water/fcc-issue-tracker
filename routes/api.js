2b0ac70c7ee2c9a5c96ffbc5f622a9c1c7648e6a
Glitch (marshy-nerve) <none>
2019-10-22 20:52:05 +0000

    🌼👢 Checkpoint ./routes/api.js:6684026/1398

--- routes/api.js
+++ routes/api.js
@@ -43,25 +43,32 @@ module.exports = function (app) {
           delete req.body[item]
         }
       })
-      Issues.findById(req.body._id)
-      .then(issue => {
-        if(issue != null){
-          Issues.findByIdAndUpdate(req.body._id, {
-            $set: req.body
-          }, {new: true})
-          .then(issue => {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.json(issue)
-          })
-          .catch(err => next(err))
-        }else{
-          let err = new Error('Issue with id: ' + req.body._id + ' not found.')
-          res.statusCode = 404;
-          return next(err);
-        }
-      })
-      .catch(err => next(err))
+      let id = req.body._id
+      delete req.body._id
+      if(Object.keys(req.body).length == 0){
+        res.send()
+      }
+      // Issues.findById(req.body._id)
+      // .then(issue => {
+      //   if(issue != null){
+      //     Issues.findByIdAndUpdate(req.body._id, {
+      //       $set: req.body
+      //     }, {new: true})
+      //     .then(issue => {
+      //       res.statusCode = 200;
+      //       res.setHeader('Content-Type', 'application/json');
+      //       res.json("Successfully updated.")
+      //     })
+      //     .catch(err => next(err))
+      //   }else{
+      //     res.statusCode = 404;
+      //     let err = new Error('Could not update: ' + req.body._id)
+      //     res.json(err);
+      //   }
+      // })
+      // .catch(err => {
+      //   res.send('Could not update: ' + req.body._id);
+      // })
     })
     
     .delete(function (req, res, next){
