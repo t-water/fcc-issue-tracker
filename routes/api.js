f35a013ce83a14992767a5d948ce21f7e66364b0
Glitch (marshy-nerve) <none>
2019-10-22 19:52:01 +0000

    🎹☔️ Checkpoint ./routes/api.js:6684026/956

--- routes/api.js
+++ routes/api.js
@@ -22,24 +22,44 @@ module.exports = function (app) {
 
   app.route('/api/issues/:project')
   
-    .get(function (req, res){
+    .get(function (req, res, next){
       var project = req.params.project;
       
     })
     
-    .post(function (req, res){
+    .post(function (req, res, next){
       Issues.create(req.body)
       .then(issue => {
+        res.statusCode = 200;
+        res.setHeader('Content Type', 'application/json');
         res.json(issue);
       })
+      .catch(err => next(err))
     })
     
-    .put(function (req, res){
-      var project = req.params.project;
-      
+    .put(function (req, res, next){
+      Issues.findById(req.body._id)
+      .then(issue => {
+        if(issue != null){
+          Issues.findByIdAndUpdate(req.body._id, {
+            $set: req.body
+          }, {new: true})
+          .then(issue => {
+            res.statusCode = 200;
+            res.setHeader('Content Type', 'application/json');
+            res.json(issue)
+          })
+          .catch(err => next(err))
+        }else{
+          let err = new Error('Issue with id: ' + req.body._id + ' not found.')
+          res.statusCode = 404;
+          return next(err);
+        }
+      })
+      .catch(err => next(err))
     })
     
-    .delete(function (req, res){
+    .delete(function (req, res, next){
       var project = req.params.project;
       
     });
