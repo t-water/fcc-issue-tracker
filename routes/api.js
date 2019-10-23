c7ca613035351571dcbee4c547738be3ac88181d
Glitch (marshy-nerve) <none>
2019-10-23 01:30:16 +0000

    🐓⛺️ Checkpoint ./routes/api.js:6684026/310

--- routes/api.js
+++ routes/api.js
@@ -29,7 +29,7 @@ module.exports = function (app) {
     
     .post(function (req, res, next){
       var project = req.params.project;
-      let Issues = mongoose.model(project, issueSchema)
+      let Issues = mongoose.model(project, issueSchema, project)
       Issues.create(req.body)
       .then(issue => {
         res.statusCode = 200;
@@ -39,62 +39,64 @@ module.exports = function (app) {
       .catch(err => next(err))
     })
     
-//     .put(function (req, res, next){
-//       var project = req.params.project;
-//       Object.keys(req.body).forEach(item => {
-//         if(req.body[item] === ""){
-//           delete req.body[item]
-//         }
-//       })
-//       let id = req.body._id
-//       delete req.body._id
-//       if(Object.keys(req.body).length == 0){
-//         res.send("No updated field sent")
-//       }
-//       Issues.findById(id)
-//       .then(issue => {
-//         if(issue != null){
-//           Issues.findByIdAndUpdate(id, {
-//             $set: req.body
-//           }, {new: true})
-//           .then(issue => {
-//             res.statusCode = 200;
-//             res.setHeader('Content-Type', 'application/text');
-//             res.send("Successfully updated.");
-//           })
-//           .catch(err => res.send('Could not update: ' + id))
-//         }else{
-//           res.send('Could not update: ' + id);
-//         }
-//       })
-//       .catch(err => {
-//         res.send('Could not update: ' + id);
-//       })
-//     })    
+    .put(function (req, res, next){
+      var project = req.params.project;
+      Object.keys(req.body).forEach(item => {
+        if(req.body[item] === ""){
+          delete req.body[item]
+        }
+      })
+      let id = req.body._id
+      delete req.body._id
+      if(Object.keys(req.body).length == 0){
+        res.send("No updated field sent")
+      }
+      let Issues = mongoose.model(project, issueSchema, project)
+      Issues.findById(id)
+      .then(issue => {
+        if(issue != null){
+          Issues.findByIdAndUpdate(id, {
+            $set: req.body
+          }, {new: true})
+          .then(issue => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/text');
+            res.send("Successfully updated.");
+          })
+          .catch(err => res.send('Could not update: ' + id))
+        }else{
+          res.send('Could not update: ' + id);
+        }
+      })
+      .catch(err => {
+        res.send('Could not update: ' + id);
+      })
+    })    
   
-//     .delete(function (req, res, next){
-//       var project = req.params.project;
-//       if(req.body._id == ""){
-//         res.send('_id error');
-//       }
-//       Issues.findById(req.body._id)
-//       .then(issue => {
-//         if(issue != null){
-//           Issues.findByIdAndRemove(req.body._id)
-//           .then(issue => {
-//             res.statusCode = 200;
-//             res.setHeader('Content-Type', 'application/text');
-//             res.send("Deleted " + req.body._id);
-//           })
-//           .catch(err => {
-//             res.send('Could not delete ' + req.body._id)
-//           })
-//         }else{
-//           res.send('Could not delete ' + req.body._id)
-//         }
-//       })
-//       .catch(err => {
-//         res.send('Could not delete ' + req.body._id)
-//       })
-//     });
+    .delete(function (req, res, next){
+      var project = req.params.project;
+      if(req.body._id == ""){
+        res.send('_id error');
+      }
+      let Issues = mongoose.model(project, issueSchema, project)
+      Issues.findById(req.body._id)
+      .then(issue => {
+        if(issue != null){
+          Issues.findByIdAndRemove(req.body._id)
+          .then(issue => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/text');
+            res.send("Deleted " + req.body._id);
+          })
+          .catch(err => {
+            res.send('Could not delete ' + req.body._id)
+          })
+        }else{
+          res.send('Could not delete ' + req.body._id)
+        }
+      })
+      .catch(err => {
+        res.send('Could not delete ' + req.body._id)
+      })
+    });
 };
